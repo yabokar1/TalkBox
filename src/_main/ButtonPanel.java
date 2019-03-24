0128888f1051fd7152c0021930a5c064859b91b1
Eric <erictpham22@gmail.com>
2019-03-23 20:51:20 -0400

    Added Images to Buttons

--- src/_main/ButtonPanel.java
+++ src/_main/ButtonPanel.java
@@ -6,11 +6,18 @@ import java.util.Observer;
 import audio_players.AudioClip;
 import audio_players.AudioClipWav;
 import io.AudioFileIO;
+import io.ImportFiles;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
@@ -38,6 +45,7 @@ public class ButtonPanel extends GridPane implements Observer{
 			
 		   Button button = new Button(name);
 		   button.setMinSize(BTN_WIDTH, BTN_HEIGHT);
+		   button.setMaxSize(BTN_WIDTH, BTN_HEIGHT);
 		   this.setHgrow(button, Priority.ALWAYS);
 		   this.setHgrow(button, Priority.ALWAYS);
 		   this.setVgap(20);
@@ -46,13 +54,6 @@ public class ButtonPanel extends GridPane implements Observer{
 		   
 		   attachClickListener(name, button);
 
-			
-		
-		   
-		    
-		
-		   
-		   
 		   this.add(button, currentCol, currentRow);
 
 		   currentCol++;
@@ -68,32 +69,28 @@ public class ButtonPanel extends GridPane implements Observer{
 		
 		
 
-
-
-
-
-		private void attachClickListener(String name, Button button) {
-			
-			
-			button.setOnAction(new EventHandler() {
-				   
+		private void attachClickListener(String name, Button b) {
+			b.setOnMouseClicked(new EventHandler<MouseEvent>() {
 				private AudioClipWav clip = null;
-
-				@Override
-				public void handle(Event event) {
-					
-			
-					clip = new AudioClipWav(name);
-					
-					clip.play();
-				}
-				   
-			   });
+	            @Override
+	            public void handle(MouseEvent event) {
+	                MouseButton button = event.getButton();
+	                if(button==MouseButton.PRIMARY) {
+						clip = new AudioClipWav(name);
+						clip.play();
+	                }else if(button==MouseButton.SECONDARY){
+	                ImportFiles image = new ImportFiles();
+	                image.open("Images/");
+	                Image pic = new Image(image.file.toURI().toString());
+	                ImageView iv = new ImageView(pic);
+	                iv.fitWidthProperty().bind(b.widthProperty());
+	                iv.fitHeightProperty().bind(b.heightProperty());
+	                b.setGraphic(iv);
+	                }
+	            }
+	        });
 		}
 
-
-		
-		
 
 		@Override
 		public void update(Observable o, Object arg) {
