f24323537298b5fc8a07a3de5f105c98fd9b3701
Neharika Puri <neharikapurieng@gmail.com>
2019-04-01 07:02:58 -0400

    Dimensions

--- src/TalkBox/src/_main/Names.java
+++ src/TalkBox/src/_main/Names.java
@@ -2,25 +2,30 @@ package _main;
 
 public class Names {
 
-	
+	// Screen Title 
 	public static  final String TITLE = "TalkBox";
 	
+	// Scene Dimensions 
 	public static final double SCENE_WIDTH = 1200;
 	
 	public static final double SCENE_HEIGHT = 600;
-	
+
+	// Launch Button Dimensions
 	public static final double LAUNCHBUTTON_WIDTH = 40;
 	
 	public static final double LAUNCHBUTTON_HEIGHT = 40;
 	
+	// Start Button Dimensions 	
 	public static final double STARTBUTTON_HEIGHT = 25;
 	
 	public static final double STARTBUTTON_WIDTH = 50;
 	
+	// Record Button Dimensions
 	public static final double RECORDBUTTON_HEIGHT = 75;
 	
 	public static final double RECORDBUTTON_WIDTH =  75;
 	
+	// Max Columns 
 	static final int MAX_COL = 6;
 	
 }
